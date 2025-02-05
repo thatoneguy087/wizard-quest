0630acb33147f40734c6fad13f73059ef7b2c588
thatoneguy087 <110648049+thatoneguy087@users.noreply.github.com>
2025-02-05 12:28:28 -0700

    Test appendchild and text on interactive elements

--- script.js
+++ script.js
@@ -1,53 +1,70 @@
 'use strict'
-function game() {
-  let choice;
-  console.log('Welcome to wizard quest!')
-  let gameStart = prompt("Start the game? (0 = no, 1 = yes");
-  if (gameStart == 1) {
-    console.log('You wake up in total darkness. It smells weird and the floor is wet');
-    console.log('1 - Try and walk around in the dark\n2 - Cast a fire spell for light\n2 - Take a nap');
-    choice = prompt('Make your move');
-    if (choice == 1) {
-      console.log('You try walking around in the dark and fall down a hole. Both of your legs are broken and youre bleeding out')
-    } else if (choice == 2) {
-      console.log('You cast fire and notice a peculiar hole in the ground. "good thing I didnt try walking in the dark." You notice the path ahead splits into two paths. Which path do you take?')
-    } else if (choice == 3) {
-      console.log('You go to bed and have strange visions. A gorilla riding a unicycle? Really? Oh my god hes looking at you now. What do you do?')
-    } else {
-      console.log('Bad decision. Your actions have incurred the wrath of the Krondosmodius, god of choose-your-own-adventure games. You have been sent to 1000 years of purgatory');
-    }
+// function game() {
+//   let choice;
+//   console.log('Welcome to wizard quest!')
+//   let gameStart = prompt("Start the game? (0 = no, 1 = yes");
+//   if (gameStart == 1) {
+//     console.log('You wake up in total darkness. It smells weird and the floor is wet');
+//     console.log('1 - Try and walk around in the dark\n2 - Cast a fire spell for light\n2 - Take a nap');
+//     choice = prompt('Make your move');
+//     if (choice == 1) {
+//       console.log('You try walking around in the dark and fall down a hole. Both of your legs are broken and youre bleeding out')
+//     } else if (choice == 2) {
+//       console.log('You cast fire and notice a peculiar hole in the ground. "good thing I didnt try walking in the dark." You notice the path ahead splits into two paths. Which path do you take?')
+//     } else if (choice == 3) {
+//       console.log('You go to bed and have strange visions. A gorilla riding a unicycle? Really? Oh my god hes looking at you now. What do you do?')
+//     } else {
+//       console.log('Bad decision. Your actions have incurred the wrath of the Krondosmodius, god of choose-your-own-adventure games. You have been sent to 1000 years of purgatory');
+//     }
 
-  } else {
-    console.log('Awww. Maybe next time.');
-  }
-}
+//   } else {
+//     console.log('Awww. Maybe next time.');
+//   }
+// }
 
-function gameOver() {
-  console.log('GAME OVER. REFRESH THE PAGE TO RESTART.');
-}
+// function gameOver() {
+//   console.log('GAME OVER. REFRESH THE PAGE TO RESTART.');
+// }
 
-function noDecision() {
-  'Bad decision. Your actions have incurred the wrath of the Krondosmodius, god of choose-your-own-adventure games. You have been sent to 1000 years of purgatory';
+// function noDecision() {
+//   'Bad decision. Your actions have incurred the wrath of the Krondosmodius, god of choose-your-own-adventure games. You have been sent to 1000 years of purgatory';
 
-}
+// }
 
-// console.log(game());
-const promptContainer = document.querySelector('.prompt-container');
-const textArea = document.querySelector('.text-area');
-const gameArea = document.querySelector('.game-area');
-const selectionArea = document.querySelector('.selection-container');
-let test = document.createElement('p');
-let test2 = document.createElement('button');
+// const promptContainer = document.querySelector('.prompt-container');
+// const textArea = document.querySelector('.text-area');
+// const gameArea = document.querySelector('.game-area');
+// const selectionArea = document.querySelector('.selection-container');
+// let test = document.createElement('p');
+// let test2 = document.createElement('button');
+
+
 const gamePrompt = document.querySelector('#game-prompt');
-
+const selectionArea = document.querySelector('.selection-container');
+console.log(selectionArea);
 gamePrompt.textContent = 'test';
-// let textContent = document.createTextNode('This is a test!')
-// let buttonContent = document.createTextNode('This is a button!')
-// test.appendChild(textContent);
-// test2.appendChild(buttonContent);
 
+function game() {
+  let playerChoice;
+  let choice1 = document.createElement('button');
+  choice1.classList.add('1');
+  choice1.textContent = 'test';
 
-// promptContainer.appendChild(test);
-// selectionArea.appendChild(test2);
-
+  gamePrompt.textContent = 'You wake up in total darkness. It smells weird and the floor is wet';
+  selectionArea.appendChild(choice1);
   
+}
+
+function path1() {
+
+}
+
+function path2() {
+
+}
+
+function path3() {
+
+}
+
+game();
