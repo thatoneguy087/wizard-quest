52818e59e0575150f55df4f9c2506c91ef2266d3
thatoneguy087 <110648049+thatoneguy087@users.noreply.github.com>
2025-02-05 18:49:01 -0700

    Update variable and class names

--- script2.js
+++ script2.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const gameContainer = document.querySelector('.game-container');
+const gameOverText = document.getElementById('game-over');
+const gamePrompt = document.querySelector('#prompt');
+const choiceSelection = document.querySelector('.choice-selection');
+gameOverText.style.visibility = 'hidden';
+
+const gameStart = [
+  0,
+  0,
+  'wakeup',
+  'stepforward',
+  'light',
+  'nap'
+]
